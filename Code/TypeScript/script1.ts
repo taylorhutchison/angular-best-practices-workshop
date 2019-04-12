932da75f67500389b9190633be31aa4a59ae9ea3
Taylor Hutchison <seanthutchison@gmail.com>
2019-04-12 13:01:58 -0500

    Add code folder

---
+++ Code/TypeScript/script1.ts
@@ -0,0 +1,12 @@
+
+var x = 'This is a very long string that takes up too many columns and that makes it sometimes hard to read. Please consider shorter strings.';
+var names = ['Alex', 'Bob', 'Chloe'];
+var num = 5;
+
+function moreThanTwo(list: string[] | string) {
+    return list.length > 2;
+}
+
+console.log(moreThanTwo(x));
+console.log(moreThanTwo(names));
+console.log(moreThanTwo(num));
